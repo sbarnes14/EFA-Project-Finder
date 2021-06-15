7e08ed6dd9946ec47338efde5d381f15eb1fbd62
theMagnificentJay <reedjoshua408@gmail.com>
2021-06-15 12:58:13 -0500

    Removed testing migrations for clean slate

--- ProjectFinder.Data/Migrations/202106151755033_initial migration.cs
+++ ProjectFinder.Data/Migrations/202106151755033_initial migration.cs
@@ -1,26 +1,58 @@
-namespace ProjectFinder.Data.Migrations
+﻿namespace ProjectFinder.Data.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class initialmigration : DbMigration
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.Course",
+                c => new
+                    {
+                        CourseId = c.Int(nullable: false, identity: true),
+                        OwnerId = c.Guid(nullable: false),
+                        Cohort = c.String(nullable: false),
+                        CourseType = c.Int(nullable: false),
+                        StartDate = c.DateTimeOffset(nullable: false, precision: 7),
+                        EndDate = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.CourseId);
+            
             CreateTable(
                 "dbo.Project",
                 c => new
                     {
                         ProjectId = c.Int(nullable: false, identity: true),
+                        OwnerId = c.Guid(nullable: false),
                         ProjectName = c.String(nullable: false),
                         ProjectDescription = c.String(nullable: false),
-                        Students = c.Int(nullable: false),
-                        CourseId = c.Int(nullable: false),
-                        GitHubRepo = c.String(nullable: false),
                         StartDate = c.DateTimeOffset(nullable: false, precision: 7),
                         EndDate = c.DateTimeOffset(nullable: false, precision: 7),
+                        StudentId = c.Int(nullable: false),
+                        CourseId = c.Int(nullable: false),
                     })
-                .PrimaryKey(t => t.ProjectId);
+                .PrimaryKey(t => t.ProjectId)
+                .ForeignKey("dbo.Course", t => t.CourseId, cascadeDelete: true)
+                .ForeignKey("dbo.Student", t => t.StudentId, cascadeDelete: true)
+                .Index(t => t.StudentId)
+                .Index(t => t.CourseId);
+            
+            CreateTable(
+                "dbo.Student",
+                c => new
+                    {
+                        StudentId = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        CourseId = c.Int(nullable: false),
+                        GithubProfile = c.String(),
+                        EnrollDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.StudentId)
+                .ForeignKey("dbo.Course", t => t.CourseId, cascadeDelete: true)
+                .Index(t => t.CourseId);
             
             CreateTable(
                 "dbo.IdentityRole",
@@ -100,16 +132,24 @@ namespace ProjectFinder.Data.Migrations
             DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
             DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.Project", "StudentId", "dbo.Student");
+            DropForeignKey("dbo.Student", "CourseId", "dbo.Course");
+            DropForeignKey("dbo.Project", "CourseId", "dbo.Course");
             DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
             DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.Student", new[] { "CourseId" });
+            DropIndex("dbo.Project", new[] { "CourseId" });
+            DropIndex("dbo.Project", new[] { "StudentId" });
             DropTable("dbo.IdentityUserLogin");
             DropTable("dbo.IdentityUserClaim");
             DropTable("dbo.ApplicationUser");
             DropTable("dbo.IdentityUserRole");
             DropTable("dbo.IdentityRole");
+            DropTable("dbo.Student");
             DropTable("dbo.Project");
+            DropTable("dbo.Course");
         }
     }
 }
--- ProjectFinder.Data/Migrations/Configuration.cs
+++ ProjectFinder.Data/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-namespace ProjectFinder.Data.Migrations
+﻿namespace ProjectFinder.Data.Migrations
 {
     using System;
     using System.Data.Entity;
@@ -10,23 +10,14 @@ namespace ProjectFinder.Data.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "EFA_Project_Finder.ApplicationDbContext";
         }
 
         protected override void Seed(EFA_Project_Finder.Data.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
         }
     }
 }
